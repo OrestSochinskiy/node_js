acfc666e49be9bb4d5c4a2377caece179d540eaf
TarasDzikovskyi <tarasdz12367@gmail.com>
2021-08-19 16:01:36 +0300

    homework-2

--- database/users.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    {name: 'Vasya', age: 23, gender: 'male', email: 'vasya228@gmail.com', password: 'pass1'},
-    {name: 'Dima', age: 16, gender: 'male', email: 'dima228@gmail.com', password: 'pass2'},
-    {name: 'Olya', age: 19, gender: 'female', email: 'olya228@gmail.com', password: 'pass3'},
-    {name: 'Vika', age: 24, gender: 'female', email: 'vika228@gmail.com', password: 'pass4'},
-    {name: 'Oleg', age: 20, gender: 'male', email: 'oleg228@gmail.com', password: 'pass5'},
-
-]
